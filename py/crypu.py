9a07d85cc67bd6bb0f86a65e16f0ac45fa7450ee
Suheel S Hussain <suheel_hussain@yahoo.com>
2018-10-16 12:31:53 -0400

    Cryptp utilities

---
+++ py/crypu.py
@@ -0,0 +1,38 @@
+# Python program to find SHA256 hash string of a file
+import sys
+import hashlib
+import random
+
+
+__all__ = ['compute_sha256', 'create_password']
+
+
+def compute_sha256(filename):
+    sha256_hash = hashlib.sha256()
+    with open(filename,"rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+        return sha256_hash.hexdigest()
+    return None
+
+
+def create_password(pw_length=8):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    mypw = ""
+
+    for i in range(pw_length):
+        next_index = random.randrange(len(alphabet))
+        mypw = mypw + alphabet[next_index]
+
+    # replace 1 or 2 characters with a number
+    for i in range(random.randrange(1,3)):
+        replace_index = random.randrange(len(mypw)//2)
+        mypw = mypw[0:replace_index] + str(random.randrange(10)) + mypw[replace_index+1:]
+
+    # replace 1 or 2 letters with an uppercase letter
+    for i in range(random.randrange(1,3)):
+        replace_index = random.randrange(len(mypw)//2,len(mypw))
+        mypw = mypw[0:replace_index] + mypw[replace_index].upper() + mypw[replace_index+1:]
+        
+    return mypw
